32683b720567ae857db2fb601796242da01f08c4
ggenois <guillaumegenois@yahoo.ca>
2024-10-25 15:04:46 -0400

    ajout comparaison avec cas de base bayes naive et correction erreur avec verify_output

--- save_output.py
+++ save_output.py
@@ -2,9 +2,10 @@ import csv
 import os
 from datetime import datetime
 import numpy as np
+import pandas as pd
 
 
-def verify_predictions(predictions):
+def verify_submissions(predictions):
     for file in os.listdir('submissions'):
         if file.endswith('.csv'):
             with open(os.path.join('submissions', file), 'r') as f:
@@ -12,7 +13,18 @@ def verify_predictions(predictions):
             label_file = np.array([int(label.split(",")[-1].strip()) for label in lines])
             if np.all(label_file == predictions):
                 return file
-    return False
+    return None
+
+def verify_output(predictions):
+    all_files = []
+    for file in os.listdir('output'):
+        if file.endswith('.csv'):
+            with open(os.path.join('output', file), 'r') as f:
+                lines = f.readlines()[1:]
+            label_file = np.array([int(label.split(",")[-1].strip()) for label in lines])
+            if np.all(label_file == predictions):
+                all_files.append(file)
+    return all_files if len(all_files) > 0 else [None]
 
 def save_output(predictions, classifier, params, transformations):
     today = datetime.now().strftime('%Y%m%d')
@@ -28,6 +40,14 @@ def save_output(predictions, classifier, params, transformations):
             writer.writerow([i, label])
     print("predictions saved in " + path)
 
-    print("already same predictions saved in submissions?", verify_predictions(predictions))
+    print("already same predictions saved in submissions?", verify_submissions(predictions))
+    print("already same predictions saved in output?")
+    for file in verify_output(predictions):
+        print('\t', file)
+
+    print('Number of 0:', np.sum(predictions == 0))
+    print('Number of 1:', np.sum(predictions == 1))
+    print('Number of differences with bases classifier submission:', np.sum(predictions != pd.read_csv("output/output_labels_bayes_classifier.csv")['label']))
+
 
 
