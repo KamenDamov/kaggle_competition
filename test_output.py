a47602eef9cecc81c6ffb6f18c5cc4a3c5916595
ggenois <guillaumegenois@yahoo.ca>
2024-10-26 10:47:18 -0400

    modif pour voir differences

--- test_output.py
+++ test_output.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df_1 = pd.read_csv("output/output_labels_bayes_classifier_part_3.csv")
-df_2 = pd.read_csv("output/output_labels_bayes_classifier.csv")
+df_1 = pd.read_csv("output/20241025/182936_comp_bayes_alpha=0.07900000000000001_stopwords.csv")
+df_2 = pd.read_csv("submissions/0.73538_output_labels_bayes_classifier.csv")
 
-print((df_1 == df_2)["label"].sum())
+print((df_1 != df_2)["label"].sum())
