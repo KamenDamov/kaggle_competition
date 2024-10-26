da2c92ce163891c156e5fc3ef907aac38ed71cb3
ggenois <guillaumegenois@yahoo.ca>
2024-10-26 10:46:14 -0400

    ajout sklearn

--- lasso_ridge_classifier.py
+++ lasso_ridge_classifier.py
@@ -11,11 +11,10 @@ from save_output import save_output
 
 
 def grid_search(X, y):
-    # TODO: need to change hyperparameters
-    alpha = np.arange(-1, 10, 2)
+    alpha = np.arange(50, 200, 10)
     param_grid = dict(alpha=alpha)
     print(param_grid)
-    cv = StratifiedShuffleSplit(n_splits=1, test_size=0.3, random_state=42)
+    cv = StratifiedShuffleSplit(n_splits=2, test_size=0.2, random_state=42)
     grid = GridSearchCV(RidgeClassifier(), param_grid=param_grid, cv=cv, scoring='f1', verbose=3)
     grid.fit(X, y)
     return grid.best_params_, grid.best_score_
@@ -32,15 +31,18 @@ if __name__ == '__main__':
     train = data_preprocess.train
     label_train = data_preprocess.label_train
     test = data_preprocess.test
+    print('train:', train.shape)
+    print('test:', test.shape)
+    print('Ratio of 1 in train:', np.sum(label_train == 1) / len(label_train))
 
-    # best_params_, best_score_ = grid_search(train, label_train)
-    # print('best params: {}, score: {}'.format(best_params_, best_score_))
+    best_params_, best_score_ = grid_search(train, label_train)
+    print('best params: {}, score: {}'.format(best_params_, best_score_))
 
-    classifier = RidgeClassifier(alpha=1.0, solver="sparse_cg")
+    classifier = RidgeClassifier(alpha=best_params_['alpha'], solver="sparse_cg")
     classifier.fit(train, label_train)
     print('f1_score on train:', f1_score(label_train, classifier.predict(train)))
 
     y_pred = classifier.predict(test)
 
-    params = f'C={classifier.alpha}'
+    params = f'alpha={classifier.alpha}'
     save_output(y_pred, "ridge", params, "")
