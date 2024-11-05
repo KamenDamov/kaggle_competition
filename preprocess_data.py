12c75c6d17ef932b9ae06776f8fe053a89519793
Guigui031 <guillaumegenois@yahoo.ca>
2024-11-05 13:31:33 -0500

    added stemming and lemming

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,8 +1,10 @@
 # import numpy as nnp
 import numpy as np
+from nltk import WordNetLemmatizer
 
 # import truncated_svd
 from sklearn.decomposition import PCA, TruncatedSVD
+from nltk.stem import SnowballStemmer
 
 # import cupy as np
 nnp = np
@@ -89,6 +91,60 @@ class DataPreprocess:
         self.train = np.delete(self.train, idx_stopwords, axis=1)
         self.test = np.delete(self.test, idx_stopwords, axis=1)
 
+    def apply_stemming(self):
+        stemmer = SnowballStemmer("english")
+
+        # Create a mapping of stemmed words to their original indices
+        stemmed_word_mapping = {}
+        for idx, word in enumerate(self.vocab_map):
+            stemmed_word = stemmer.stem(word)
+            if stemmed_word not in stemmed_word_mapping:
+                stemmed_word_mapping[stemmed_word] = []
+            stemmed_word_mapping[stemmed_word].append(idx)
+
+        # Create a new combined matrix
+        # The number of unique stemmed words will determine the new matrix shape
+        unique_stems = list(stemmed_word_mapping.keys())
+        new_train = np.zeros((self.train.shape[0], len(unique_stems)))
+        new_test = np.zeros((self.test.shape[0], len(unique_stems)))
+
+        # Fill the new matrix with summed occurrences
+        for stem, indices in stemmed_word_mapping.items():
+            for idx in indices:
+                new_train[:, unique_stems.index(stem)] += self.train[:, idx]
+                new_test[:, unique_stems.index(stem)] += self.test[:, idx]
+
+        self.train = new_train
+        self.test = new_test
+        self.vocab_map = np.array(unique_stems)
+
+    def apply_lemmatization(self):
+        # Initialize the WordNet Lemmatizer
+        lemmatizer = WordNetLemmatizer()
+
+        # Create a mapping of lemmatized words to their original indices
+        lemmatized_word_mapping = {}
+        for idx, word in enumerate(self.vocab_map):
+            lemmatized_word = lemmatizer.lemmatize(word)  # You may specify the POS if needed
+            if lemmatized_word not in lemmatized_word_mapping:
+                lemmatized_word_mapping[lemmatized_word] = []
+            lemmatized_word_mapping[lemmatized_word].append(idx)
+
+        # Create a new combined matrix for lemmatized words
+        unique_lemmas = list(lemmatized_word_mapping.keys())
+        new_train = np.zeros((self.train.shape[0], len(unique_lemmas)))
+        new_test = np.zeros((self.test.shape[0], len(unique_lemmas)))
+
+        # Fill the new matrix with summed occurrences
+        for lemma, indices in lemmatized_word_mapping.items():
+            for idx in indices:
+                new_train[:, unique_lemmas.index(lemma)] += self.train[:, idx]
+                new_test[:, unique_lemmas.index(lemma)] += self.test[:, idx]
+
+        self.train = new_train
+        self.test = new_test
+        self.vocab_map = np.array(unique_lemmas)
+
     def apply_pca(self, n_components):
         # self.train, _, _ = pca.incremental_pca(self.train, 500, n_components)
         # self.test, _, _ = pca.incremental_pca(self.test, 500, n_components)
@@ -122,11 +178,23 @@ if __name__ == "__main__":
     # print('vocab_map', dataPrepocess.vocab_map.shape)
     # print('label_train', dataPrepocess.label_train.shape)
 
-    dataPrepocess.apply_truncated_svd(2000)
-    print('train', dataPrepocess.train.shape)
-    print('test', dataPrepocess.test.shape)
+    # dataPrepocess.apply_truncated_svd(2000)
+    # print('train', dataPrepocess.train.shape)
+    # print('test', dataPrepocess.test.shape)
 
     # dataPrepocess.initialize_tfidf()
     # dataPrepocess.remove_min_max(5, 0.999)
     # print('train', dataPrepocess.train.shape)
     # print('test', dataPrepocess.test.shape)
+
+    dataPrepocess.apply_stemming()
+    print('train', dataPrepocess.train.shape)
+    print('test', dataPrepocess.test.shape)
+    print('vocab_map', dataPrepocess.vocab_map.shape)
+    print('label_train', dataPrepocess.label_train.shape)
+
+    dataPrepocess.apply_lemmatization()
+    print('train', dataPrepocess.train.shape)
+    print('test', dataPrepocess.test.shape)
+    print('vocab_map', dataPrepocess.vocab_map.shape)
+    print('label_train', dataPrepocess.label_train.shape)
