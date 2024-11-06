3aebe12953e869e8a200bb6c3a9941f32fa21727
Guillaume Genois <49683548+Guigui031@users.noreply.github.com>
2024-11-06 09:52:56 -0500

    Update save_output.py

--- save_output.py
+++ save_output.py
@@ -2,9 +2,10 @@ import csv
 import os
 from datetime import datetime
 import numpy as np
+import pandas as pd
 
 
-def verify_predictions(predictions):
+def verify_submissions(predictions):
     for file in os.listdir('submissions'):
         if file.endswith('.csv'):
             with open(os.path.join('submissions', file), 'r') as f:
@@ -12,7 +13,20 @@ def verify_predictions(predictions):
             label_file = np.array([int(label.split(",")[-1].strip()) for label in lines])
             if np.all(label_file == predictions):
                 return file
-    return False
+    return None
+
+def verify_output(predictions, newly_saved):
+    all_files = []
+    for root, dirs, files in os.walk('output'):
+        for file in files:
+            if file.endswith('.csv') and os.path.join(root, file) != newly_saved:
+                with open(os.path.join(root, file), 'r') as f:
+                    lines = f.readlines()[1:]
+                label_file = np.array([int(label.split(",")[-1].strip()) for label in lines])
+                if np.all(label_file == predictions):
+                    all_files.append(os.path.join(root, file))
+
+    return all_files if len(all_files) > 0 else [None]
 
 def save_output(predictions, classifier, params, transformations):
     today = datetime.now().strftime('%Y%m%d')
@@ -28,6 +42,15 @@ def save_output(predictions, classifier, params, transformations):
             writer.writerow([i, label])
     print("predictions saved in " + path)
 
-    print("already same predictions saved in submissions?", verify_predictions(predictions))
+    print("already same predictions saved in submissions?", verify_submissions(predictions))
+    print("already same predictions saved in output?")
+    for file in verify_output(predictions, path):
+        print('\t', file)
 
+    print('Number of 0:', np.sum(predictions == 0))
+    print('Number of 1:', np.sum(predictions == 1))
+    print('Ratio of 1:', np.sum(predictions == 1) / len(predictions))
 
+    bayes_submission = pd.read_csv("output/output_labels_bayes_classifier.csv")['label']
+    print('Number of differences with bayes classifier submission:', np.sum(predictions != bayes_submission))
+    print('Ratio of 1 in bayes submission:', np.sum(bayes_submission == 1) / len(bayes_submission))
