023929a055822c05c5ea8ca5ac4fd121bde96454
KamenDamov <kamendamov123@gmail.com>
2024-11-07 20:40:14 -0500

    Faible variance entre modeles ensemblistes

--- test_output.py
+++ test_output.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 champion = pd.read_csv("output/20241104/134731_ensemble_cnb_xgb_logreg_random_search_stopwords_undersampled.csv")
 runner_up = pd.read_csv("184703_comp_bayes_alpha0.761_stopwords.csv")
-candidat = pd.read_csv("output/20241107/140736_cnb_random_search_50_iter_cumulative_sum_undersampled.csv")
-
+candidat = pd.read_csv("output/20241107/175012_ensemble_cnb_xgboost_logreg_random_search_15_iter_stopwords_undersampled.csv")
 
 print((champion != candidat)["label"].sum())
