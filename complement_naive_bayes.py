e18e96496188b4cf5f3a6c4e61cf61931dcd7e04
KamenDamov <kamendamov123@gmail.com>
2024-11-10 18:35:17 -0500

    WIP - Notebook, debug saev_output.py

--- complement_naive_bayes.py
+++ complement_naive_bayes.py
@@ -10,8 +10,8 @@ from sklearn.preprocessing import FunctionTransformer
 def train_cnb_with_tfidf(X_train, y_train) -> ComplementNB:
     # TFIDF génèrent de moins bonnes perfo sur validation
     pipeline = Pipeline([
-        #('tfidf', TfidfTransformer()),   
-        #('to_float32', FunctionTransformer(lambda X: X.astype(np.float32))),           
+        ('tfidf', TfidfTransformer()),   
+        ('to_float32', FunctionTransformer(lambda X: X.astype(np.float32))),           
         ('cnb', ComplementNB())                    
     ])
 
@@ -23,8 +23,35 @@ def train_cnb_with_tfidf(X_train, y_train) -> ComplementNB:
     scorer = make_scorer(f1_score)
 
     random_search = RandomizedSearchCV(
-        pipeline, param_distributions, scoring=scorer, cv=5, n_jobs=1, 
-        verbose=3, n_iter=50, random_state=42
+        pipeline, param_distributions, scoring=scorer, cv=2, n_jobs=1, 
+        verbose=3, n_iter=1, random_state=42
+    )
+
+    random_search.fit(X_train, y_train)
+
+    best_params = random_search.best_params_
+    print("Best Parameters:", best_params)
+    print("Best F1 Score:", random_search.best_score_)
+    best_cnb = random_search.best_estimator_
+    tfidf_transformer = best_cnb.named_steps['tfidf']
+    return best_cnb, tfidf_transformer
+
+
+def train_cnb(X_train, y_train) -> ComplementNB:
+    pipeline = Pipeline([
+        ('cnb', ComplementNB())                    
+    ])
+
+    # Define parameter grid for both TF-IDF and ComplementNB
+    param_distributions = {
+        'cnb__alpha': uniform(0.0001, 1.5) 
+    }
+
+    scorer = make_scorer(f1_score)
+
+    random_search = RandomizedSearchCV(
+        pipeline, param_distributions, scoring=scorer, cv=2, n_jobs=1, 
+        verbose=3, n_iter=1, random_state=42
     )
 
     random_search.fit(X_train, y_train)
@@ -34,3 +61,5 @@ def train_cnb_with_tfidf(X_train, y_train) -> ComplementNB:
     print("Best F1 Score:", random_search.best_score_)
     best_cnb = random_search.best_estimator_
     return best_cnb
+
+
--- main.py
+++ main.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     #visualize_data.check_sparsity_decrease(data_preprocess.train)
 
     #indeces_to_remove = remove_cum_sum(np.concatenate((data_preprocess.train, data_preprocess.test), axis=0), 0.99)
-    indeces_to_remove = remove_cum_sum(data_preprocess.train, 0.95)
-    data_preprocess.train = np.delete(data_preprocess.train, indeces_to_remove, axis=1)
-    data_preprocess.test = np.delete(data_preprocess.test, indeces_to_remove, axis=1)
+    #indeces_to_remove = remove_cum_sum(data_preprocess.train, 0.95)
+    #data_preprocess.train = np.delete(data_preprocess.train, indeces_to_remove, axis=1)
+    #data_preprocess.test = np.delete(data_preprocess.test, indeces_to_remove, axis=1)
 
     X_train_undersampled, y_train_undersampled = random_undersampling(data_preprocess.train, data_preprocess.label_train)
 
